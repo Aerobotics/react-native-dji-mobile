9e589b02d22c51a1071db4256e4f97368fb7bf35
Adam Rosendorff <arosendorff@users.noreply.github.com>
2019-05-15 11:08:55 +0200

    feat: Begin implementation of data logger class.

---
+++ ios/DJIRealTimeDataLogger.swift
@@ -0,0 +1,21 @@
+//
+//  DroneRealTimeDataLogger.swift
+//  ReactNativeDjiMobile
+//
+//  Created by Adam Rosendorff on 2019/05/15.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import DJISDK
+
+class DJIRealTimeDataLogger {
+  
+  func startLogging(filename: String) {
+    
+  }
+  
+  func stopLogging() {
+    
+  }
+}
