cf8987e62ff5ebd953517d38a4ba90844c3119af
Adam Rosendorff <arosendorff@users.noreply.github.com>
2019-05-30 13:55:51 +0200

    feat: Unify SDK events into single event handler class.

---
+++ android/src/main/java/com/aerobotics/DjiMobile/CameraEventDelegate.java
@@ -0,0 +1,119 @@
+package com.aerobotics.DjiMobile;
+
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
+
+import dji.common.camera.SystemState;
+import dji.common.error.DJIError;
+import dji.keysdk.CameraKey;
+import dji.keysdk.DJIKey;
+import dji.keysdk.KeyManager;
+import dji.keysdk.ProductKey;
+import dji.keysdk.callback.GetCallback;
+import dji.keysdk.callback.KeyListener;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.media.MediaFile;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+// The CameraEventDelegate assumes that the app has successfully registered, otherwise things may crash!
+public class CameraEventDelegate implements SystemState.Callback, MediaFile.Callback {
+
+  private class CameraEventObservable extends Observable {
+    public void sendEvent(Object payload) {
+      setChanged();
+      notifyObservers(payload);
+    }
+  }
+
+  private CameraEventObservable cameraEventObservable = new CameraEventObservable();
+
+  private CameraEventDelegate cameraDelegateSenderInstance = this;
+
+  private boolean isProductConnected = false;
+  private boolean isCameraConnected = false;
+
+  private KeyListener ProductConnectedKeyListener = new KeyListener() {
+    @Override
+    public void onValueChange(@Nullable Object oldValue, @Nullable Object newValue) {
+      if (newValue != null && newValue instanceof Boolean) {
+        isProductConnected = (boolean)newValue;
+        setCameraCallbacks();
+      }
+    }
+  };
+
+  private KeyListener CameraConnectedKeyListener = new KeyListener() {
+    @Override
+    public void onValueChange(@Nullable Object oldValue, @Nullable Object newValue) {
+      if (newValue != null && newValue instanceof Boolean) {
+        isCameraConnected = (boolean)newValue;
+        setCameraCallbacks();
+      }
+    }
+  };
+
+  CameraEventDelegate() {
+    // Sometimes the camera component is reported as connected before the base product, so we need to ensure that both are connected before
+    // attempting to access the camera via the base product.
+    KeyManager keyManager = DJISDKManager.getInstance().getKeyManager();
+//    keyManager.addListener(ProductKey.create(ProductKey.CONNECTION), ProductConnectedKeyListener);
+    keyManager.addListener(CameraKey.create(CameraKey.CONNECTION), CameraConnectedKeyListener);
+  }
+
+
+
+  private void postEvent(SDKEvent sdkEvent, Object eventData) {
+    HashMap payload = new HashMap();
+    payload.put("eventType", sdkEvent);
+    payload.put("value", eventData);
+    cameraEventObservable.sendEvent(payload);
+  }
+
+  private void setCameraCallbacks() {
+    if (isCameraConnected) {
+
+      // When the SDK has just registered, calling getProduct may return null, so wait a second before trying
+      Handler handler = new Handler();
+      handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Camera camera = DJISDKManager.getInstance().getProduct().getCamera();
+            Log.i("REACT", "SETTING CALLBACKS!");
+
+            // Add additional callbacks here as required
+            camera.setSystemStateCallback(cameraDelegateSenderInstance);
+            camera.setMediaFileCallback(cameraDelegateSenderInstance);
+        }
+      }, 1000);
+
+
+
+
+    }
+  }
+
+  public void addObserver(Observer observer) {
+    cameraEventObservable.addObserver(observer);
+  }
+
+  public void removeObserver(Observer observer) {
+    cameraEventObservable.deleteObserver(observer);
+  }
+
+  @Override
+  public void onUpdate(@NonNull SystemState systemState) {
+    this.postEvent(SDKEvent.CameraDidUpdateSystemState, systemState);
+  }
+
+  @Override
+  public void onNewFile(@NonNull MediaFile mediaFile) {
+    this.postEvent(SDKEvent.CameraDidGenerateNewMediaFile, mediaFile);
+  }
+}
---
+++ android/src/main/java/com/aerobotics/DjiMobile/SdkEventHandler.java
@@ -0,0 +1,117 @@
+package com.aerobotics.DjiMobile;
+
+
+import android.support.annotation.Nullable;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
+
+import dji.keysdk.BatteryKey;
+import dji.keysdk.DJIKey;
+import dji.keysdk.FlightControllerKey;
+import dji.keysdk.ProductKey;
+import dji.keysdk.callback.KeyListener;
+import dji.sdk.media.MediaFile;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+enum EventType {
+  DJI_KEY_MANAGER_EVENT,
+  DJI_CAMERA_DELEGATE_EVENT,
+}
+
+enum SDKEvent {
+  ProductConnection(ProductKey.create(ProductKey.CONNECTION), EventType.DJI_KEY_MANAGER_EVENT),
+  BatteryChargeRemaining(BatteryKey.create(BatteryKey.CHARGE_REMAINING_IN_PERCENT), EventType.DJI_KEY_MANAGER_EVENT),
+  AircraftCompassHeading(FlightControllerKey.create(FlightControllerKey.COMPASS_HEADING), EventType.DJI_KEY_MANAGER_EVENT),
+  AircraftLocation(FlightControllerKey.create(FlightControllerKey.AIRCRAFT_LOCATION), EventType.DJI_KEY_MANAGER_EVENT),
+  AircraftVelocityX(FlightControllerKey.create(FlightControllerKey.VELOCITY_X), EventType.DJI_KEY_MANAGER_EVENT),
+  AircraftVelocityY(FlightControllerKey.create(FlightControllerKey.VELOCITY_Y), EventType.DJI_KEY_MANAGER_EVENT),
+  AircraftVelocityZ(FlightControllerKey.create(FlightControllerKey.VELOCITY_Z), EventType.DJI_KEY_MANAGER_EVENT),
+
+  CameraDidUpdateSystemState(null, EventType.DJI_CAMERA_DELEGATE_EVENT),
+  CameraDidGenerateNewMediaFile(null, EventType.DJI_CAMERA_DELEGATE_EVENT);
+
+  private Object key;
+  private EventType eventType;
+
+  SDKEvent(Object key, EventType eventType) {
+    this.key = key;
+    this.eventType = eventType;
+  }
+
+  public Object getKey() {
+    return key;
+  }
+
+  public EventType getEventType() {
+    return eventType;
+  }
+
+}
+
+interface EventListener {
+  void onValueChange(@Nullable Object oldValue, @Nullable Object newValue);
+}
+
+public class SdkEventHandler {
+
+  private CameraEventDelegate cameraEventDelegate = new CameraEventDelegate();
+
+  private class EventInfo {
+    String key;
+    EventType eventType;
+  }
+
+  public Object startEventListener(final SDKEvent sdkEvent, final EventListener eventListener) {
+    EventType eventType = sdkEvent.getEventType();
+    Object key = sdkEvent.getKey();
+
+    if (eventType == EventType.DJI_KEY_MANAGER_EVENT) {
+      KeyListener keyListener = new KeyListener() {
+        @Override
+        public void onValueChange(@Nullable Object oldValue, @Nullable Object newValue) {
+          eventListener.onValueChange(oldValue, newValue);
+        }
+      };
+      DJISDKManager.getInstance().getKeyManager().addListener((DJIKey)key, keyListener);
+      return keyListener;
+
+    } else if (eventType == EventType.DJI_CAMERA_DELEGATE_EVENT) {
+      Observer observer = new Observer() {
+        @Override
+        public void update(Observable observable, Object newValue) {
+          if (newValue instanceof HashMap) {
+            HashMap payload = (HashMap) newValue;
+            // CameraEventDelegate sends an update for all camera events, so only send update if eventType matches sdkEvent we want
+            if (payload.get("eventType") == sdkEvent) {
+              eventListener.onValueChange(null, payload);
+            }
+          }
+
+        }
+      };
+      cameraEventDelegate.addObserver(observer);
+      return observer;
+    }
+
+    return null;
+  }
+
+  public void stopEventListener(SDKEvent SDKEvent, @Nullable Object eventSubscriptionObject) {
+    EventType eventType = SDKEvent.getEventType();
+    Object key = SDKEvent.getKey();
+
+    if (eventType == EventType.DJI_KEY_MANAGER_EVENT) {
+      DJISDKManager.getInstance().getKeyManager().removeListener((KeyListener)eventSubscriptionObject);
+
+    } else if (eventType == EventType.DJI_CAMERA_DELEGATE_EVENT) {
+      cameraEventDelegate.removeObserver((Observer)eventSubscriptionObject);
+    }
+  }
+
+
+}
