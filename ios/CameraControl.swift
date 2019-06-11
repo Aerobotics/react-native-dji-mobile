519a343ccb617446fa42fb26c10f1696ea14e5e6
Adam Rosendorff <arosendorff@users.noreply.github.com>
2019-06-11 16:52:18 +0200

    fix: Fix CameraControl for iOS.

--- ios/CameraControl.swift
+++ ios/CameraControl.swift
@@ -12,9 +12,9 @@ import DJISDK
 @objc(CameraControlNative)
 class CameraControlNative: NSObject {
   
-  @objc(setPhotoAspectRatioreso:resolve:reject:)
+  @objc(setPhotoAspectRatio:resolve:reject:)
   func setPhotoAspectRatio(photoAspectRatio: UInt, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-    DJISDKManager.keyManager()?.performAction(for: DJICameraKey(param: DJICameraParamPhotoAspectRatio)!, withArguments: [DJICameraPhotoAspectRatio.init(rawValue: photoAspectRatio)!], andCompletion: { (finished: Bool, value: DJIKeyedValue?, error: Error?) in
+    DJISDKManager.keyManager()?.setValue(photoAspectRatio, for: DJICameraKey(param: DJICameraParamPhotoAspectRatio)!, withCompletion: { (error: Error?) in
       if (error == nil) {
         resolve("CameraControl: Photo aspect ratio set successfully")
       } else {
@@ -33,7 +33,7 @@ class CameraControlNative: NSObject {
       whiteBalance = DJICameraWhiteBalance.init(customColorTemperature: colorTemperature)
     }
     
-    DJISDKManager.keyManager()?.performAction(for: DJICameraKey(param: DJICameraParamWhiteBalance)!, withArguments: [whiteBalance!], andCompletion: { (finished: Bool, value: DJIKeyedValue?, error: Error?) in
+    DJISDKManager.keyManager()?.setValue(whiteBalance, for: DJICameraKey(param: DJICameraParamWhiteBalance)!, withCompletion: { (error: Error?) in
       if (error == nil) {
         resolve("CameraControl: White balance set successfully")
       } else {
@@ -44,7 +44,7 @@ class CameraControlNative: NSObject {
   
   @objc(setExposureMode:resolve:reject:)
   func setExposureMode(exposureMode: UInt, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-    DJISDKManager.keyManager()?.performAction(for: DJICameraKey(param: DJICameraParamExposureMode)!, withArguments: [DJICameraExposureMode.init(rawValue: exposureMode)!], andCompletion: { (finished: Bool, value: DJIKeyedValue?, error: Error?) in
+    DJISDKManager.keyManager()?.setValue(exposureMode, for: DJICameraKey(param: DJICameraParamExposureMode)!, withCompletion: { (error: Error?) in
       if (error == nil) {
         resolve("CameraControl: Exposure mode set successfully")
       } else {
