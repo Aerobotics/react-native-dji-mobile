e6eaf5765260cbcb4f0add4b92b73e4a473427cb
MikeWootz <michael@aerobotics.com>
2020-03-04 16:17:49 +0200

    Add method to get gimbal attitude

--- android/src/main/java/com/aerobotics/DjiMobile/GimbalWrapper.java
+++ android/src/main/java/com/aerobotics/DjiMobile/GimbalWrapper.java
@@ -1,15 +1,23 @@
 package com.aerobotics.DjiMobile;
 
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 
 import dji.common.error.DJIError;
+import dji.common.gimbal.Attitude;
 import dji.common.gimbal.Rotation;
 import dji.common.gimbal.RotationMode;
 import dji.common.util.CommonCallbacks;
+import dji.keysdk.DJIKey;
+import dji.keysdk.GimbalKey;
+import dji.keysdk.callback.GetCallback;
 import dji.sdk.gimbal.Gimbal;
 import dji.sdk.sdkmanager.DJISDKManager;
 
@@ -59,6 +67,32 @@ public class GimbalWrapper extends ReactContextBaseJavaModule {
       }
     });
 
+  }
+
+  @ReactMethod
+  public void getGimbalAttitude(final Promise promise) {
+    DJIKey gimbalAttitudeInDegrees = GimbalKey.create(GimbalKey.ATTITUDE_IN_DEGREES);
+    DJISDKManager.getInstance().getKeyManager().getValue(gimbalAttitudeInDegrees, new GetCallback() {
+      @Override
+      public void onSuccess(@NonNull Object value) {
+        if (value instanceof Attitude) {
+          Attitude attitude = (Attitude) value;
+          float pitch = attitude.getPitch();
+          float roll = attitude.getRoll();
+          float yaw = attitude.getYaw();
+          WritableMap params = Arguments.createMap();
+          params.putDouble("pitch", pitch);
+          params.putDouble("roll", roll);
+          params.putDouble("yaw", yaw);
+          promise.resolve(params);
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull DJIError djiError) {
+        promise.reject(new Throwable(djiError.getDescription()));
+      }
+    });
   }
 
   @Override
