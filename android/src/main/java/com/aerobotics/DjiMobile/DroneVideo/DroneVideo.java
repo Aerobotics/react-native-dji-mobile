9bf698e2f9a8dd7c39b9df81d07512010e9eb2d5
Nick Anderson <nick.anderson@aerobotics.com>
2021-09-03 15:39:49 +0200

    fix: switch camera feed to left index instead of FPV

--- android/src/main/java/com/aerobotics/DjiMobile/DroneVideo/DroneVideo.java
+++ android/src/main/java/com/aerobotics/DjiMobile/DroneVideo/DroneVideo.java
@@ -2,26 +2,60 @@ package com.aerobotics.DjiMobile.DroneVideo;
 
 import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.aerobotics.DjiMobile.R;
 
+import dji.common.airlink.PhysicalSource;
+import dji.common.error.DJIError;
+import dji.common.util.CommonCallbacks;
 import dji.midware.usb.P3.UsbAccessoryService;
+import dji.sdk.base.BaseProduct;
 import dji.sdk.camera.VideoFeeder;
 import dji.sdk.codec.DJICodecManager;
 import dji.sdk.sdkmanager.DJISDKManager;
+import dji.sdk.airlink.AirLink;
+import dji.sdk.airlink.OcuSyncLink;
 
 public class DroneVideo extends RelativeLayout implements TextureView.SurfaceTextureListener {
 
   private DJICodecManager codecManager;
+  private AirLink airLink;
 
   public DroneVideo(Context context) {
     super(context);
+    initAirLink();
+    setPrimaryVideoFeed();
     initializeSurfaceTexture();
   }
 
+  private void initAirLink() {
+    BaseProduct baseProduct = DJISDKManager.getInstance().getProduct();
+    // TODO: it seems like the product has not been initialised when this code is
+    // initially run. Need to use a production connection callback here.
+    if (baseProduct != null && baseProduct.getAirLink() != null) {
+      airLink = baseProduct.getAirLink();
+    }
+  }
+
+  private void setPrimaryVideoFeed() {
+    if (airLink != null) {
+      OcuSyncLink ocuSyncLink = airLink.getOcuSyncLink();
+      if (ocuSyncLink != null) {
+        ocuSyncLink.assignSourceToPrimaryChannel(PhysicalSource.LEFT_CAM, PhysicalSource.FPV_CAM, new CommonCallbacks.CompletionCallback() {
+          @Override
+          public void onResult(DJIError error) {
+            if (error != null) {
+              Log.d("REACT", "Failed to set video feed primary channel: " + error.getDescription());
+            }
+          }
+        });
+      }
+    }
+  }
   private void initializeSurfaceTexture() {
     View.inflate(getContext(), R.layout.drone_video_layout, this);
     TextureView droneVideoTexture = findViewById(R.id.droneVideoTexture);
