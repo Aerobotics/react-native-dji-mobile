840dfb105dcb7772c1163f53c29ff1283d1d9b07
Nick Anderson <nick.anderson@aerobotics.com>
2022-04-19 17:43:59 +0200

    fix: fix types for home location

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,93 @@
-export type DJIEvent = { type: string; value: any }
-
-export type LocationCoordinate3D = { latitude: number, longitude: number, altitude: number }
-export type LocationCoordinate2D = { latitude: number, longitude: number }
-export type VelocityVector = { x: number, y: number, z: number }
-export type Attitude = { roll: number, pitch: number, yaw: number }
-export type WaypointMissionState = 'NOT_SUPPORTED' | 'READY_TO_UPLOAD' | 'UPLOADING' | 'READY_TO_EXECUTE' | 'EXECUTING' | 'EXECUTION_PAUSED' | 'DISCONNECTED' | 'RECOVERING' | 'UNKNOWN'
-export type WaypointMissionExecuteState = 'INITIALIZING' | 'MOVING' | 'CURVE_MODE_MOVING' | 'CURVE_MODE_TURNING' | 'BEGIN_ACTION' | 'DOING_ACTION' | 'FINISHED_ACTION' | 'RETURN_TO_FIRST_WAYPOINT' | 'PAUSED'
-export interface WaypointMissionExecutionProgressEvent {
-  targetWaypointIndex: number,
-  isWaypointReached: boolean,
-  executeState: WaypointMissionExecuteState,
+export interface DJIEvent {
+  type: string;
+  value: any;
 }
+
+export interface HomeLocationCoordinate3D {
+  latitude?: number;
+  longitude?: number;
+  altitude?: number;
+}
+
+export interface LocationCoordinate3D {
+  latitude: number;
+  longitude: number;
+  altitude: number;
+}
+
+export interface LocationCoordinate2D {
+  latitude: number;
+  longitude: number;
+}
+
+export interface VelocityVector {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface Attitude {
+  roll: number;
+  pitch: number;
+  yaw: number;
+}
+
+export type WaypointMissionState =
+  | 'NOT_SUPPORTED'
+  | 'READY_TO_UPLOAD'
+  | 'UPLOADING'
+  | 'READY_TO_EXECUTE'
+  | 'EXECUTING'
+  | 'EXECUTION_PAUSED'
+  | 'DISCONNECTED'
+  | 'RECOVERING'
+  | 'UNKNOWN';
+
+export type WaypointMissionExecuteState =
+  | 'INITIALIZING'
+  | 'MOVING'
+  | 'CURVE_MODE_MOVING'
+  | 'CURVE_MODE_TURNING'
+  | 'BEGIN_ACTION'
+  | 'DOING_ACTION'
+  | 'FINISHED_ACTION'
+  | 'RETURN_TO_FIRST_WAYPOINT'
+  | 'PAUSED';
+
+export interface WaypointMissionExecutionProgressEvent {
+  targetWaypointIndex: number;
+  isWaypointReached: boolean;
+  executeState: WaypointMissionExecuteState;
+}
+
 export interface WaypointMissionUploadEvent {
-  uploadedWaypointIndex: number,
-  totalWaypointCount: number,
-  isSummaryUploaded: boolean,
+  uploadedWaypointIndex: number;
+  totalWaypointCount: number;
+  isSummaryUploaded: boolean;
+}
+
+export const FlightLogListenerEventNames = Object.freeze({
+  create: 'create',
+  modify: 'modify',
+});
+
+export interface FlightLogListenerEvent {
+  value: {
+    eventName: typeof FlightLogListenerEventNames,
+    fileName: string,
+  };
+  type: string;
+}
+
+export interface MediaFileData {
+  fileName: string;
+  fileSizeInBytes: number;
+  dateCreated: string;
+};
+
+export interface DJIDiagnostic {
+  type: string;
+  reason: string;
+  solution: string;
+  error: string;
 }
