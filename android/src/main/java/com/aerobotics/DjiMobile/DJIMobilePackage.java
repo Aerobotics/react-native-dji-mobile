69eadfdea742f5a20f6f556a965da64b0b50f7d7
Nick Anderson <nick.anderson@aerobotics.com>
2022-07-05 16:44:08 +0200

    feat: choose primary video feed for connected drone

--- android/src/main/java/com/aerobotics/DjiMobile/DJIMobilePackage.java
+++ android/src/main/java/com/aerobotics/DjiMobile/DJIMobilePackage.java
@@ -2,12 +2,10 @@ package com.aerobotics.DjiMobile;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import com.aerobotics.DjiMobile.DroneVideo.DroneVideoManager;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
--- android/src/main/java/com/aerobotics/DjiMobile/DroneVideo/DroneVideoManager.java
+++ android/src/main/java/com/aerobotics/DjiMobile/DroneVideo/DroneVideoManager.java
@@ -1,17 +1,63 @@
 package com.aerobotics.DjiMobile.DroneVideo;
 
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import org.jetbrains.annotations.NotNull;
+
+import dji.common.airlink.PhysicalSource;
 
 public class DroneVideoManager extends SimpleViewManager<DroneVideo> {
 
+  @NotNull
   @Override
   public String getName() {
     return "DroneVideo";
   }
 
+  @NotNull
   @Override
-  protected DroneVideo createViewInstance(ThemedReactContext reactContext) {
+  protected DroneVideo createViewInstance(@NotNull ThemedReactContext reactContext) {
     return new DroneVideo(reactContext);
+  }
+
+  /*
+   *  FIXME: Could not get this to work as desired. I could not update the video feed once it has
+   *  already been rendered. So for now, the choosing of the video feed is handled by the component
+   */
+  @ReactProp(name = "videoSrc")
+  public void setVideoSrc(DroneVideo view, @Nullable String videoSrc) {
+    if (videoSrc == null) {
+      return;
+    }
+
+    switch (videoSrc) {
+      case "MODEL_MAIN_CAMERA":
+        view.setPrimaryVideoFeedForModel();
+        break;
+      case "LEFT_CAMERA":
+        view.setPrimaryVideoFeed(PhysicalSource.LEFT_CAM);
+        break;
+      case "FPV_CAMERA":
+        view.setPrimaryVideoFeed(PhysicalSource.FPV_CAM);
+        break;
+      case "MAIN_CAMERA":
+        view.setPrimaryVideoFeed(PhysicalSource.MAIN_CAM);
+        break;
+      case "RIGHT_CAMERA":
+        view.setPrimaryVideoFeed(PhysicalSource.RIGHT_CAM);
+        break;
+      case "UNKNOWN":
+        view.setPrimaryVideoFeed(PhysicalSource.UNKNOWN);
+        break;
+      default:
+        Log.w("REACT", "Unknown video src" + videoSrc);
+        break;
+    }
   }
 }
